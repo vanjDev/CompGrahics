d6bb2ff3cb3308319b343498f48fa982eebf020b
Vanj <143190477+vanjDev@users.noreply.github.com>
2025-09-25 12:17:44 +0800

    Create Labaratory4.cpp

---
+++ Labaratory4.cpp
@@ -0,0 +1,30 @@
+#include <GL/glut.h>
+
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glLineWidth(4.0f);
+	glEnable(GL_LINE_STIPPLE);
+	glLineStipple(1, 0xFF00);
+
+	glBegin(GL_LINES);
+
+	glVertex2f(0.0f, 0.8f); glVertex2f(-0.8f, -0.4f);
+	glVertex2f(0.0f, 0.8f); glVertex2f(0.8f, -0.4f);
+	glVertex2f(-0.8f, -0.4f); glVertex2f(0.8f, -0.4f);
+
+	glVertex2f(0.0f, -0.8f); glVertex2f(0.8f, 0.4f);
+	glVertex2f(0.0f, -0.8f); glVertex2f(-0.8f, 0.4f);
+	glVertex2f(0.8f, 0.4f); glVertex2f(-0.8f, 0.4f);
+
+	glEnd();
+	glFlush();
+}
+
+int main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitWindowSize(500, 500);
+	glutCreateWindow("Labaratory 3");
+	glutDisplayFunc(display);
+	glutMainLoop();
+	return 0;
+}
